3c60417a06896119f4b5b54c1549c43c8ab4a296
salman-AS <salmans581@gmail.com>
2024-01-02 17:53:38 +0530

    3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,8 @@
-const reverseString = function() {
-
+const reverseString = function(word) {
+    let revWord = '';
+    for(let i=word.length-1;i>=0;i--)
+        revWord += word.charAt(i);
+    return revWord;
 };
 
 // Do not edit below this line
