32fa99d502658b3c3033f86b071d4600e73d24cc
salman-AS <salmans581@gmail.com>
2024-01-02 18:55:44 +0530

    done 4 5 6 7

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,10 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...args) {
+    const arr2 = [];
+    for (const item of array){
+        if(!args.includes(item))
+            arr2.push(item);
+    }
+    return arr2;
 };
 
 // Do not edit below this line
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,16 @@
-const sumAll = function() {
-
+const sumAll = function (first, last) {
+    if (typeof first != "number" || typeof last != "number" || last < 0 || first < 0)
+        return "ERROR";
+    if (first > last) {
+        let temp = first;
+        first = last;
+        last = temp;
+    }
+    let sum = 0;
+    for (let i = first; i <= last; i++) {
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
